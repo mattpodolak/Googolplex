7c68d8c335c73fa083ee62f2957b3885fb68fef2
mattpodolak <mpodola2@uwo.ca>
2017-11-04 15:16:15 -0400

    Created pseduo-recursive method for summarizing

--- summary.py
+++ summary.py
@@ -11,12 +11,14 @@ from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
 
 LANGUAGE = "english"
-SENTENCES_COUNT = 4
+SENTENCES_COUNT = 20
 
 
 if __name__ == "__main__":
     url = "http://www.encyclopedia.com/plants-and-animals/plants/plants/potato"
     parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
+
+    print(parser)
     # or for plain text files
     # parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
     stemmer = Stemmer(LANGUAGE)
@@ -33,9 +35,35 @@ if __name__ == "__main__":
     summarizer_Edmundson.null_words = get_stop_words(LANGUAGE)
     summarizer_Edmundson.bonus_words = parser.significant_words
     summarizer_Edmundson.stigma_words = parser.stigma_words
-
     summary_Edmundson = summarizer_Edmundson(parser.document, SENTENCES_COUNT)
 
+    #store summaries in a text  file
+    list_of_sums = [summary_Lsa, summary_LexRank, summary_Edmundson]
+    f = open('summarized.txt', 'w')
+    for t in list_of_sums:
+        line = ' '.join(str(x) for x in t)
+        f.write(line + '\n')
+    f.close()
+
+    #create new shorter summaries
+    parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
+    SENTENCES_COUNT = 3
+
+    #define summarizers for the summarizing methods being used
+    summarizer_Lsa = Lsa(stemmer)
+    summarizer_Lsa.stop_words = get_stop_words(LANGUAGE)
+    summary_Lsa = summarizer_Lsa(parser.document, SENTENCES_COUNT)
+
+    summarizer_LexRank = LexRank()
+    summary_LexRank = summarizer_LexRank(parser.document, SENTENCES_COUNT)
+
+    summarizer_Edmundson = Edmundson(stemmer)
+    summarizer_Edmundson.null_words = get_stop_words(LANGUAGE)
+    summarizer_Edmundson.bonus_words = parser.significant_words
+    summarizer_Edmundson.stigma_words = parser.stigma_words
+    summary_Edmundson = summarizer_Edmundson(parser.document, SENTENCES_COUNT)
+
+    #print summaries
     for sentence in summary_Lsa:
         print(sentence)
     print('\n')
