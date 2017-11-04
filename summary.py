4521ee758d2332d220b8ab04782f3c6beffe8032
mattpodolak <mpodola2@uwo.ca>
2017-11-04 14:28:53 -0400

    Initialize summarizing algorithms

---
+++ summary.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
+
+from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Lsa
+from sumy.summarizers.edmundson import EdmundsonSummarizer as Edmundson
+from sumy.summarizers.lex_rank import LexRankSummarizer as LexRank
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+
+LANGUAGE = "english"
+SENTENCES_COUNT = 4
+
+
+if __name__ == "__main__":
+    url = "http://www.encyclopedia.com/plants-and-animals/plants/plants/potato"
+    parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
+    # or for plain text files
+    # parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
+    stemmer = Stemmer(LANGUAGE)
+
+    #define summarizers for the summarizing methods being used
+    summarizer_Lsa = Lsa(stemmer)
+    summarizer_Lsa.stop_words = get_stop_words(LANGUAGE)
+    summary_Lsa = summarizer_Lsa(parser.document, SENTENCES_COUNT)
+
+    summarizer_LexRank = LexRank()
+    summary_LexRank = summarizer_LexRank(parser.document, SENTENCES_COUNT)
+
+    summarizer_Edmundson = Edmundson(stemmer)
+    summarizer_Edmundson.stop_words = get_stop_words(LANGUAGE)
+    summary_Edmundson = summarizer_Edmundson(parser.document, SENTENCES_COUNT)
+
+    for sentence in summary_Lsa:
+        print(sentence)
+    print('\n')
+    for sentence in summary_LexRank:
+        print(sentence)
