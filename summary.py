39ef51704085accf7f7426b27912c9f2862bfcb6
mattpodolak <mpodola2@uwo.ca>
2017-11-05 02:35:48 -0500

    Pull data from the form and enter into keywords alg

--- summary.py
+++ summary.py
@@ -11,8 +11,11 @@ from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
 from sumy.evaluation.rouge import rouge_n, rouge_1, rouge_2
 from sumy.models.dom import Sentence, Paragraph, ObjectDocumentModel
-
-from flask import Flask, render_template
+import nltk
+from nltk.corpus import stopwords
+from itertools import product
+from nltk.corpus import wordnet as wn
+from flask import Flask, render_template, request, json
 app = Flask(__name__)
 
 @app.route("/")
@@ -49,7 +52,43 @@ def max_r_value(Lsa_eval, Ed_eval, Lex_eval, ref):
     print("Maximum average Rouge test value " + str(max))
     return maxIndex
 
+@app.route('/keywordCall', methods=['POST'])
+def keywordCall():
+    # read the posted values from the UI
+    _input = request.form['inputSearch']
+    print(_input)
+    #keyword(str(_input))
+    return render_template('index.html')
 
+def keyword(input):
+    # temp sentence
+    temp_s = input
+    # timer
+    #start = timeit.default_timer()
+    # tokenize
+    token_s = nltk.word_tokenize(temp_s)
+    # remove stopwords
+    stopWords = set(stopwords.words('english'))
+    filtered_tokens = [word for word in token_s if word not in stopWords]
+    filtered_tokens = []
+    for word in token_s:
+        if word not in stopWords:
+            filtered_tokens.append(word)
+    # ini print
+    print('tokens:', token_s)
+    print('filtered:', filtered_tokens)
+    # loops through for comparisons
+    filtered_iter = iter(filtered_tokens)
+    for i in range(len(filtered_tokens)):
+        total = 0
+        elem = next(filtered_iter)
+        allsyns1 = set(ss for word in token_s for ss in wn.synsets(word))
+        allsyns2 = set(ss for word in elem for ss in wn.synsets(word))
+        full_list = [(wn.wup_similarity(s1, s2) or 0, s1, s2) for s1, s2 in product(allsyns1, allsyns2)]
+        score_list = iter(full_list)
+        for j in range(len(full_list)):
+            total += float(next(score_list)[0])
+        print('word:', elem, 'weighted avg:', total/len(full_list))
 
 if __name__ == "__main__":
     app.run()
