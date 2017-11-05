677f5641e076f8a19fc359ba76378b071cb4e0d0
mattpodolak <mpodola2@uwo.ca>
2017-11-04 20:57:39 -0400

    finished rouge n testing and implementation

--- summary.py
+++ summary.py
@@ -10,6 +10,7 @@ from sumy.summarizers.lex_rank import LexRankSummarizer as LexRank
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
 from sumy.evaluation.rouge import rouge_n, rouge_1, rouge_2
+from sumy.models.dom import Sentence, Paragraph, ObjectDocumentModel
 
 LANGUAGE = "english"
 SENTENCES_COUNT = 4
@@ -25,6 +26,23 @@ def calc_value(eval_sentences, ref_sentences):
     print('n3 ' + str(n_3) + '\n')
     print('avg ' + str((n_1+n_2+n_3)/3))
 
+    return (n_1+n_2+n_3)/3
+
+def max_r_value(Lsa_eval, Ed_eval, Lex_eval, ref):
+    Ed = calc_value(Ed_eval, ref)
+    Lsa = calc_value(Lsa_eval, ref)
+    Lex = calc_value(Lex_eval, ref)
+    list=[Ed, Lsa, Lex]
+    max = 0
+    #returns index of max avg rogue_n val 0=Ed, 1=Lsa, 2=Lex
+    for i in range(len(list)):
+        if list[i] > max:
+            max = list[i]
+            maxIndex = i
+    return maxIndex
+
+
+
 if __name__ == "__main__":
     url = "http://www.encyclopedia.com/plants-and-animals/plants/plants/potato"
     #url = "http://www.encyclopedia.com/plants-and-animals/plants/plants/cabbage"
@@ -34,6 +52,7 @@ if __name__ == "__main__":
 
     #create a list of reference sentences to calculate ROUGE_N scores
     ref_sentences = []
+    trim_ref_sentences = []
     for paragraph in parser._article.main_text:
         for sections in paragraph:
             for sentences in sections:
@@ -45,6 +64,7 @@ if __name__ == "__main__":
                     #catch type errors caused by annotated text ie h1, b, etc
                     print("typeError")
                     continue
+    trim_ref_sentences.extend(Sentence(s, Tokenizer(LANGUAGE)) for s in ref_sentences)
 
     # or for plain text files
     # parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
@@ -99,7 +119,7 @@ if __name__ == "__main__":
             summary_Lsa_trim.append(sentence)
 
     #calc rouge_n scores
-    calc_value(summary_Lsa_trim, ref_sentences)
+    calc_value(summary_Lsa_trim, trim_ref_sentences)
 
     print('\n')
     summary_LexRank_trim = []
@@ -110,7 +130,7 @@ if __name__ == "__main__":
             summary_LexRank_trim.append(sentence)
 
     #calc rouge_n scores
-    calc_value(summary_LexRank_trim, ref_sentences)
+    calc_value(summary_LexRank_trim, trim_ref_sentences)
 
     print('\n')
     summary_Edmundson_trim = []
@@ -121,4 +141,9 @@ if __name__ == "__main__":
             summary_Edmundson_trim.append(sentence)
 
     #calc rouge_n scores
-    calc_value(summary_Edmundson_trim, ref_sentences)
+    calc_value(summary_Edmundson_trim, trim_ref_sentences)
+
+    # returns index of max 0=Ed, 1=Lsa, 2=Lex
+    models = {0: "Edmundson Model", 1: "Lsa Model", 2:"LexRank Model"}
+    best_summary = max_r_value(summary_Lsa_trim, summary_LexRank_trim, summary_Edmundson_trim, trim_ref_sentences)
+    print(models.get(best_summary) + ' is the best model according to an average of the Rouge_3, 2 and 1 tests')
