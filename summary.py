476c2582417828f4252ced2194b10226275785ea
mattpodolak <mpodola2@uwo.ca>
2017-11-05 04:02:57 -0500

    added query section and references to html injection function

--- summary.py
+++ summary.py
@@ -16,12 +16,16 @@ from nltk.corpus import stopwords
 from itertools import product
 from nltk.corpus import wordnet as wn
 from flask import Flask, render_template, request, json
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as BS
+
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template('index.html')
 
+#constants for summary
 LANGUAGE = "english"
 SENTENCES_COUNT = 4
 
@@ -108,20 +112,45 @@ def keyword(input):
         comp_list.pop(curr_max)
 
         # prints top 3
-    print('TOP:', top_3)
+    #print('TOP:', top_3)
+    query(top_3)
     #html_inj(top_3[0],html_id)
-   # html_inj(top_3[1], html_id)
+    #html_inj(top_3[1], html_id)
     #html_inj(top_3[2], html_id)
-
-if __name__ == "__main__":
-    app.run()
-    url = "http://www.encyclopedia.com/plants-and-animals/plants/plants/potato"
-    #url = "http://www.encyclopedia.com/plants-and-animals/plants/plants/cabbage"
-    #url = "http://www.encyclopedia.com/medicine/diseases-and-conditions/pathology/accident"
-    #url = "http://www.encyclopedia.com/earth-and-environment/atmosphere-and-weather/atmospheric-and-space-sciences-atmosphere/air"
+def query(keywords):
+    #checks wikipedia for an article about the keyword
+    #uses beautiful soup to see if the following string is in the html
+    # if it is in the html - it signifies that no page was found for
+    #the corresponding keyword, in this case return "no articles" to html
+    #in the case an article is found, summarize and inject in html
+    urls = []
+    count = 0
+    for i in keywords:
+        urls.append('https://en.wikipedia.org/wiki/'+i)
+        page = urlopen(urls[count])
+        soup = BS(page, 'html.parser')
+        string = soup.find('b')
+        if not string == "Wikipedia does not have an article with this exact name.":
+            summary(urls[count])
+        elif count == 0:
+            #html_inj('No articles found', html_id)
+            print(count)
+        elif count == 1:
+            #html_inj('No articles found', html_id)
+            print(count)
+        elif count == 2:
+            #html_inj('No articles found', html_id)
+            print(count)
+        count +=1
+def summary(article_url):
+    url = article_url
+    #url = "http://www.encyclopedia.com/plants-and-animals/plants/plants/potato"
+    # url = "http://www.encyclopedia.com/plants-and-animals/plants/plants/cabbage"
+    # url = "http://www.encyclopedia.com/medicine/diseases-and-conditions/pathology/accident"
+    # url = "http://www.encyclopedia.com/earth-and-environment/atmosphere-and-weather/atmospheric-and-space-sciences-atmosphere/air"
     parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
 
-    #create a list of reference sentences to calculate ROUGE_N scores
+    # create a list of reference sentences to calculate ROUGE_N scores
     ref_sentences = []
     trim_ref_sentences = []
     for paragraph in parser._article.main_text:
@@ -132,7 +161,7 @@ if __name__ == "__main__":
                         # trim off super short - likely a few word sentences
                         ref_sentences.append(sentences)
                 except TypeError:
-                    #catch type errors caused by annotated text ie h1, b, etc
+                    # catch type errors caused by annotated text ie h1, b, etc
                     print("typeError")
                     continue
     trim_ref_sentences.extend(Sentence(s, Tokenizer(LANGUAGE)) for s in ref_sentences)
@@ -141,7 +170,7 @@ if __name__ == "__main__":
     # parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
     stemmer = Stemmer(LANGUAGE)
 
-    #define summarizers for the summarizing methods being used
+    # define summarizers for the summarizing methods being used
     summarizer_Lsa = Lsa(stemmer)
     summarizer_Lsa.stop_words = get_stop_words(LANGUAGE)
     summary_Lsa = summarizer_Lsa(parser.document, SENTENCES_COUNT)
@@ -155,40 +184,42 @@ if __name__ == "__main__":
     summarizer_Edmundson.stigma_words = parser.stigma_words
     summary_Edmundson = summarizer_Edmundson(parser.document, SENTENCES_COUNT)
 
-    #print summaries
+    # print summaries
     summary_Lsa_trim = []
     for sentence in summary_Lsa:
-        #trim off super short - likely a few word sentences
-        if len(sentence._text)>20:
+        # trim off super short - likely a few word sentences
+        if len(sentence._text) > 20:
             print(sentence)
             summary_Lsa_trim.append(sentence)
 
-    #calc rouge_n scores
+    # calc rouge_n scores
     calc_value(summary_Lsa_trim, trim_ref_sentences)
 
     print('\n')
     summary_LexRank_trim = []
     for sentence in summary_LexRank:
-        #trim off super short - likely a few word sentences
-        if len(sentence._text)>20:
+        # trim off super short - likely a few word sentences
+        if len(sentence._text) > 20:
             print(sentence)
             summary_LexRank_trim.append(sentence)
 
-    #calc rouge_n scores
+    # calc rouge_n scores
     calc_value(summary_LexRank_trim, trim_ref_sentences)
 
     print('\n')
     summary_Edmundson_trim = []
     for sentence in summary_Edmundson:
-        #trim off super short - likely a few word sentences
-        if len(sentence._text)>20:
+        # trim off super short - likely a few word sentences
+        if len(sentence._text) > 20:
             print(sentence)
             summary_Edmundson_trim.append(sentence)
 
-    #calc rouge_n scores
+    # calc rouge_n scores
     calc_value(summary_Edmundson_trim, trim_ref_sentences)
 
     # returns index of max 0=Ed, 1=Lsa, 2=Lex
-    models = {0: "Edmundson Model", 1: "Lsa Model", 2:"LexRank Model"}
+    models = {0: "Edmundson Model", 1: "Lsa Model", 2: "LexRank Model"}
     best_summary = max_r_value(summary_Lsa_trim, summary_LexRank_trim, summary_Edmundson_trim, trim_ref_sentences)
     print(models.get(best_summary) + ' is the best model according to an average of the Rouge_3, 2 and 1 tests')
+if __name__ == "__main__":
+    app.run()
